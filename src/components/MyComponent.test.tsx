6294fc52613f4be97d9c29aff82e0fdd63a1b493
Laura Hall <podinjshmi1993@gmail.com>
2023-08-29 11:41:19 +0100

    setting up for deployment

--- src/components/MyComponent.test.tsx
+++ src/components/MyComponent.test.tsx
@@ -3,9 +3,9 @@ import { MyComponent } from "./MyComponent";
 
 //An example of using react-testing-library
 describe("MyComponent", async () => {
-    test("Should have text Hello from My Component on it", () => {
+    test("Should have text Hello from Ho Kei and Laura on it", () => {
         render(<MyComponent />);
-        const elem = screen.getByText("Hello from My Component");
+        const elem = screen.getByText("Hello from Ho Kei and Laura");
         expect(elem).toBeInTheDocument();
     });
 });
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,3 +1,3 @@
 export function MyComponent(): JSX.Element {
-    return <div>Hello from My Component</div>;
+    return <div>Hello from Ho Kei and Laura</div>;
 }
