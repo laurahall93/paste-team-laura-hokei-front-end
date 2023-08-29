353139ff62962ba9121816e8c9aa3e55e2009aad
Your Name <you@example.com>
2023-08-29 15:22:20 +0100

    implemented first load GRY request:temp text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,44 @@
 import Header from "./projectHeader";
 import Footer from "./projectFooter";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { baseUrl } from "../utils/baseUrl";
 import "./App.css";
 
 function App(): JSX.Element {
+    //on click submit button - should update
+    const [recentSubmits, setRecentSubmits] = useState("");
+
+    //firstload:
+    async function fetchAllSubmit(endpoint: string) {
+        try {
+            const reponse = await axios.get(`${baseUrl}${endpoint}`);
+            // const reponse = await axios.get(
+            //     "https://jsonplaceholder.typicode.com/todos"
+            // );
+
+            const submits = reponse.data;
+            setRecentSubmits(submits.msg);
+            console.log(submits);
+            console.log("fetched all recentSubmits");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        // populate data on first load
+        fetchAllSubmit("/");
+    }, []);
+
     return (
         <div className="App">
             <Header />
             <div>
                 <h2>Main body goes here</h2>
+                <h3>1- input area</h3>
+                <h3>2- List view</h3>
+                <div>{recentSubmits}</div>
             </div>
             <Footer />
         </div>
