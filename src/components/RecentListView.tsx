81dabde46174b397d3cf8984d6be1ee7c2b50ecc
Your Name <you@example.com>
2023-08-31 13:46:08 +0100

    added button state

--- src/components/RecentListView.tsx
+++ src/components/RecentListView.tsx
@@ -9,19 +9,20 @@ export interface DataViewProps {
 
 export function DispleyRecentListView(props: DataViewProps): JSX.Element {
     //state for button default = false -> popup will not show, onclick = true ->popup shows
-    const [popup, SetPopup] = useState<string>("");
+    const [popupButton, setpopupButton] = useState<boolean>(false);
+    const [popup, setPopup] = useState<string>("");
 
     function handlePopupClick() {
+        setpopupButton(!popupButton);
         const getPopupInfo = async (endpoint: string) => {
             try {
                 const id = props.submit.id;
-
                 const response = await axios.get(`${baseUrl}${endpoint}${id}`);
                 const result = response.data;
                 console.log(
                     `fetch submit: ${id} and display as a popup, title: ${result.title}`
                 );
-                SetPopup(result[0].body);
+                setPopup(result[0].body);
                 console.log(`result: ${result[0].id}`); //result[0].id = id:2
             } catch (err) {
                 console.log(err);
@@ -29,16 +30,6 @@ export function DispleyRecentListView(props: DataViewProps): JSX.Element {
         };
         getPopupInfo("/pastes/");
     }
-
-    // function DisplayPopup(data: any): JSX.Element {
-    //     const fullBody = data[0].body;
-
-    //     return (
-    //         <div>
-    //             <p>Full summary: {fullBody}</p>
-    //         </div>
-    //     );
-    // }
 
     return (
         <div>
@@ -49,13 +40,19 @@ export function DispleyRecentListView(props: DataViewProps): JSX.Element {
                     <pre>
                         <code>{props.submit.body}</code>
                     </pre>
-                    <button onClick={handlePopupClick}>Get All Code</button>
+                    <button onClick={handlePopupClick}>
+                        {popupButton !== false ? "Close code" : "Get All Code"}
+                    </button>
                 </h4>
             </div>
             <div>
-                {/* change popup !== undefined to button state once implemented */}
-                {popup !== undefined ? (
-                    <div>Full summary: {popup}</div>
+                {popupButton === true ? (
+                    <div>
+                        Full summary:{" "}
+                        <pre>
+                            <code>{popup}</code>
+                        </pre>
+                    </div>
                 ) : (
                     <p> </p>
                 )}
